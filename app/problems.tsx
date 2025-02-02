2a37b7273943b5bcab878003e0c2df873faf3efd
SanjayRamesh <190983069+sanjayramesh2903@users.noreply.github.com>
2025-02-02 16:37:58 -0500

    Update problems.tsx

--- app/problems.tsx
+++ app/problems.tsx
@@ -1,26 +1,56 @@
 'use client'
 
-import styles from '../styles/Home.module.css'
+import { useState } from 'react'
 
-const ProblemsPage = () => {
-  const problems = [
-    { title: 'Problem 1', description: 'Description of problem 1' },
-    { title: 'Problem 2', description: 'Description of problem 2' },
-  ]
+const questions = [
+  {
+    question: 'What is the atomic number of Carbon?',
+    options: ['6', '8', '12', '14'],
+    answer: '6',
+  },
+  {
+    question: 'What is the pH of pure water?',
+    options: ['7', '0', '14', '1'],
+    answer: '7',
+  },
+]
+
+const McqPage = () => {
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null)
+  const [score, setScore] = useState(0)
+  const [questionIndex, setQuestionIndex] = useState(0)
+
+  const handleAnswer = (option: string) => {
+    if (option === questions[questionIndex].answer) {
+      setScore(score + 1)
+    }
+    if (questionIndex < questions.length - 1) {
+      setQuestionIndex(questionIndex + 1)
+    } else {
+      alert(`Your score: ${score + 1} / ${questions.length}`)
+    }
+    setSelectedAnswer(null)
+  }
 
   return (
-    <div className={styles.container}>
-      <h1>USACO Problems</h1>
-      <ul>
-        {problems.map((problem, index) => (
-          <li key={index}>
-            <h3>{problem.title}</h3>
-            <p>{problem.description}</p>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h1>Chemistry Olympiad MCQs</h1>
+      <div>
+        <p>{questions[questionIndex].question}</p>
+        <div>
+          {questions[questionIndex].options.map((option, index) => (
+            <button
+              key={index}
+              onClick={() => handleAnswer(option)}
+              style={{ margin: '5px' }}
+            >
+              {option}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
 
-export default ProblemsPage
+export default McqPage
